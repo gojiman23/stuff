042cb5409c3d02101ff3fa9d7f73b11a71b33bc9
gojiman23 <dsiv001@ucr.edu>
2018-02-06 13:09:40 -0800

    Add files via upload

---
+++ xv6/sysproc.c
@@ -0,0 +1,108 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  int exit_status;
+  argint(0, &exit_status);
+  exit(exit_status);
+  return 0;  // not reached
+}
+
+int
+sys_wait(void)
+{
+   int *perc;
+   argptr(0, (char**)&perc, sizeof(int*));
+   return wait(perc);
+}
+
+int
+sys_waitpid(void)
+{
+ int pid_2;
+ int *exit_status;
+ int options;
+
+  argint(0, &pid_2);
+  argptr(1, (char**)&exit_status, sizeof(int*));
+  argint(2, &options);
+  return waitpid(pid_2, exit_status, options); // need to implement
+}
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+int
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
